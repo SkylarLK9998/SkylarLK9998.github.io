5c345ca5711256fd66ee15cf4f1e6c5973521d06
Skylar Krieger <skylarlkrieger@gmail.com>
2019-01-05 17:00:02 -0800

    1/5/19

--- cognitivebehavioraltherapyjournal/cbtjmain.js
+++ cognitivebehavioraltherapyjournal/cbtjmain.js
@@ -1,62 +1,145 @@
-angular.module('myApp', [/*'smart-table'*/])
-    .controller('mainCtrl', ['$scope',
-        function ($scope) {
-          // $scope.rowCollection = [
-          //     {Date: new Date(), AutomaticNegativeThoughts: 'abc', CognitiveDistorion: 'Emotional Reasoning', Response: '123'},
-          //     {Date: new Date(), AutomaticNegativeThoughts: 'xyz', CognitiveDistorion: 'Shoulds', Response: '456'},
-          //     {Date: new Date(), AutomaticNegativeThoughts: 'ijk', CognitiveDistorion: 'MindReading', Response: '789'}
-          // ];
-          console.log("IDK, Something");
-        }
-    ]);
-// document.getElementById("addEntry").addEventListener("click", function(){
-// 	var JournalEntry = {
-// 		Date: new Date(),
-// 		AutomaticNegativeThoughts: document.getElementById("ANTData").value,
-// 		CognitiveDistorion: document.getElementById("CDSelection").selectedIndex,
-// 		Response: document.getElementById("ResponseData").value
-// 	};
-// });
-
-
-//addEntryBtn.addEventListener("click", addJournalEntry);
-addJournalEntry = function(){
-	var JournalEntry = {
-		Date: new Date(),
-		AutomaticNegativeThoughts: document.getElementById("ANTData").value,
-		CognitiveDistorion: document.getElementById("CDSelection").selectedIndex,
-		Response: document.getElementById("ResponseData").value
-	};
-
-	let id = `Journal-${Math.random()}`
-	localStorage.setItem(id, simpleStringify(JournalEntry));
-	console.log(localStorage)
+function newEntry(){
+  var div1 = document.getElementById("oldEntries");
+  div1.hidden = true;
+  var div = document.getElementById("newEntry");
+  div.hidden = false;
 }
-function simpleStringify (object){
-    var simpleObject = {};
-    for (var prop in object ){
-        if (!object.hasOwnProperty(prop)){
-            continue;
+
+function addJournalEntry(){
+  var CDS;
+  var CDSn = document.getElementById("CDSelection").selectedIndex
+  switch(CDSn) {
+  case 0:
+    CDS = "Mind Reading";
+    break;
+  case 1:
+    CDS = "Labeling";
+    break;
+  case 2:
+    CDS = "Predicting the Future";
+    break;
+  case 3:
+    CDS = "Shoulds";
+    break;
+  case 4:
+    CDS = "Catastrophic Thinking";
+    break; 
+  case 5:
+    CDS = "Black and White Thinking";
+    break;   
+  case 6:
+    CDS = "Emotional Reasoning";
+    break;
+  case 7:
+    CDS = "Personalization";
+    break;
+  default:
+    CDS = "Overgeneralization";
+  }
+  var JournalEntry = {
+    Date: new Date(),
+    AutomaticNegativeThoughts: document.getElementById("txt1").value,
+    CognitiveDistorion: CDS,
+    Response: document.getElementById("txt2").value
+  };
+  var entries  = JSON.parse(localStorage.getItem(entries));
+  entries += JSON.stringify(JournalEntry);
+  localStorage.setItem("entries", JSON.stringify(entries));
+}
+
+function viewOldEntries(){
+  var div1 = document.getElementById("newEntry");
+  div1.hidden = true;
+  var txtarea = document.getElementById("oes");
+  txtarea.value = parseOldEntries();
+  var div = document.getElementById("oldEntries");
+  div.hidden = false;
+}
+
+// function exportOldEntries(){
+//   var preHtml = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>Export HTML To Doc</title></head><body>";
+//   var postHtml = "</body></html>";
+//   var html = preHtml+document.getElementById(element).innerHTML+postHtml;
+
+//   var blob = new Blob(['\ufeff', html], {
+//       type: 'application/msword'
+//   });
+    
+//   // Specify link url
+//   var url = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(html);
+    
+//   // Specify file name
+//   filename = filename?filename+'.doc':'document.doc';
+    
+//   // Create download link element
+//   var downloadLink = document.createElement("a");
+
+//   document.body.appendChild(downloadLink);
+    
+//   if(navigator.msSaveOrOpenBlob ){
+//       navigator.msSaveOrOpenBlob(blob, filename);
+//   }else{
+//       // Create a link to the file
+//       downloadLink.href = url;
+      
+//       // Setting the file name
+//       downloadLink.download = filename;
+        
+//       //triggering the function
+//       downloadLink.click();
+//   }
+    
+//     document.body.removeChild(downloadLink);
+// }
+
+function parseOldEntries(){
+  var txt = "";
+  var entries  = JSON.parse(localStorage.getItem("entries"));
+  //console.log(entries);
+  var markerA = 0;
+  var markerB = 0;
+  var markerC = 0;
+  //start at i=5 to skip over the "null{" at the start of entries, end at length-2 to skip "}" at the end
+  for (var i = 5; i <= entries.length - 2; i++) {
+    if(entries[i]=='\"'){
+      if(entries[i-1]!='\\'){
+        if(markerA==0){
+          markerA=i+1; //to get rid of the '"'
+        }else{
+          markerB=i;
+          if(markerC%2==1){
+            var str = entries.substring(markerA, markerB);
+            if(markerC%8==1){
+              str = str.substring(0, 10);
+            }
+            txt += str
+          }else if(markerC%8==0){
+            txt += "\n\n\nOn "
+          }else if(markerC%8==2){
+            txt += " you had the following Automatic Negative Thoughts: \n"
+          }else if(markerC%8==4){
+            txt += "\nYou felt "
+          }else{ 
+            txt += ", here is the Realistic Alternative you came up with for those Automatic Negative Thoughts: \n"
+          }
+          markerA=0;
+          markerB=0;
+          markerC++;
         }
-        if (typeof(object[prop]) == 'object'){
-            continue;
-        }
-        if (typeof(object[prop]) == 'function'){
-            continue;
-        }
-        simpleObject[prop] = object[prop];
+      }
     }
-    return JSON.stringify(simpleObject); // returns cleaned up JSON
+  }
+  return txt;
 }
 
-window.onload = function(){
-	document.getElementById("addEntry").addEventListener("click", addJournalEntry);
-}
+// addEntryBtn.addEventListener("click", addJournalEntry);
+// addJournalEntry = function(){
+//   var JournalEntry = {
+//     Date: new Date(),
+//     AutomaticNegativeThoughts: document.getElementById("ANTData").value,
+//     CognitiveDistorion: document.getElementById("CDSelection").selectedIndex,
+//     Response: document.getElementById("ResponseData").value
+//   };
 
-setUpTable = function(){
-	var table = document.getElementById("entryLog");
-	for(i=1; i<localStorage.length(); i++){
-
-	}
-}
-// document.getElementById("addEntry").addEventListener("click", addJournalEntry());
+//   localStorage.setItem(id, simpleStringify(JournalEntry));
+// }
